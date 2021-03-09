dee9af78d35bcec6b53db6c28e09f86f4770b73f
argahutama <argahut@gmail.com>
2021-03-09 13:59:19 +0700

    refactor & add internet permission in android manifest

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:retaurant_app/data/models/models.dart';
 import 'package:retaurant_app/data/response/response.dart';
-import 'package:retaurant_app/data/services/services.dart';
+import 'package:retaurant_app/services/services.dart';
 import 'package:retaurant_app/shared/shared.dart';
 
 part 'detail_restaurant_provider.dart';
--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -5,7 +5,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:retaurant_app/data/models/models.dart';
-import 'package:retaurant_app/data/services/services.dart';
+import 'package:retaurant_app/services/services.dart';
 import 'package:retaurant_app/provider/provider.dart';
 import 'package:retaurant_app/shared/shared.dart';
 import 'package:retaurant_app/ui/widgets/widgets.dart';
