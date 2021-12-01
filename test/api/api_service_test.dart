f0cc994c88c5387ef1884bc5bbf6fa3de7f19eb4
argahutama <argahut@gmail.com>
2021-12-01 17:56:49 +0700

    fix api_service_test.dart

--- test/api/api_service_test.dart
+++ test/api/api_service_test.dart
@@ -1,10 +1,25 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:retaurant_app/services/services.dart';
+import 'package:retaurant_app/data/models/models.dart';
+
+var testRestaurant = {
+  "id": "s1knt6za9kkfw1e867",
+  "name": "Kafe Kita",
+  "description":
+      "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. ...",
+  "pictureId": "25",
+  "city": "Gorontalo",
+  "rating": 4
+};
 
 void main() {
-  test('Fetch restaurant name', () async {
-    final restoData = await ApiService().getListRestaurants();
-    var restoName = restoData.restaurants[0];
-    expect(restoName.name, "Melting Pot");
+  test("Test Parsing", () async {
+    var result = Restaurant.fromJson(testRestaurant);
+    expect(result.id, "s1knt6za9kkfw1e867");
+    expect(result.name, "Kafe Kita");
+    expect(result.description,
+        "Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. ...");
+    expect(result.pictureId, "25");
+    expect(result.city, "Gorontalo");
+    expect(result.rating, 4);
   });
 }
